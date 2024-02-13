3669c81e4bdf455a062bed4a983804bc8e3ab153
Mohamed3li454 <mohhamed4413@gmail.com>
2024-02-13 02:32:58 +0200

    update

---
+++ lib/widgets/custom_note_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class custom_node_card extends StatelessWidget {
+  const custom_node_card({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 16, bottom: 16, left: 20, right: 17),
+      // ignore: sort_child_properties_last
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            contentPadding: const EdgeInsets.all(0),
+            title: const Text(
+              "title",
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 7),
+              child: Text(
+                "subtitlesdfdsdfadsfsdafadsvdvddsavdsadsvadv dfadsfsfadfadf adsfadf",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.7), fontSize: 16),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete_outline_rounded,
+                  color: Colors.black,
+                  size: 32,
+                )),
+          ),
+          Text(
+            "date",
+            style:
+                TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 16),
+          )
+        ],
+      ),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(16),
+      ),
+    );
+  }
+}
--- lib/widgets/note_view_body.dart
+++ lib/widgets/note_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:note_app/widgets/custom_appbar.dart';
+import 'package:note_app/widgets/custom_note_card.dart';
 
 // ignore: camel_case_types
 class note_view_body extends StatelessWidget {
@@ -18,42 +18,6 @@ class note_view_body extends StatelessWidget {
           custom_appbar(),
           custom_node_card()
         ],
-      ),
-    );
-  }
-}
-
-class custom_node_card extends StatelessWidget {
-  const custom_node_card({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      // ignore: sort_child_properties_last
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text("title",
-                style: GoogleFonts.poppins(
-                    textStyle: TextStyle(color: Colors.black))),
-            subtitle: const Text(
-              "subtitle",
-              style: TextStyle(color: Colors.black),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.delete_outline_rounded,
-                  color: Colors.black,
-                )),
-          ),
-          Text("date", style: TextStyle(color: Colors.black))
-        ],
-      ),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(16),
       ),
     );
   }
